3dadd7bda1567f91cadf480d1a23b41a53e68fd1
Fahri <111134259+fahrikurniawan99@users.noreply.github.com>
2025-03-13 12:45:52 +0700

    update

--- src/service/wa-web.js
+++ src/service/wa-web.js
@@ -1,7 +1,12 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 
-const client = new Client({ authStrategy: new LocalAuth() });
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
+});
 
 client.on("qr", (qr) => {
   console.log("🔑 Scan QR Code untuk login:");
